aa5f101340c12bca332e76b5b6e64355ae6dda89
Nijat Muhtar <nijatmn@gmail.com>
2019-06-06 22:03:03 -0400

    string useful methods

--- src/test/java/string/StringExplanation.java
+++ src/test/java/string/StringExplanation.java
@@ -1,10 +1,44 @@
 package string;
 
+import java.util.Arrays;
+
 public class StringExplanation {
 
 	public static void main(String[] args) {
 		
+		//                 01234
 		
+		String apple = "Apple";
+		String grape = "apple";
+		
+		System.out.println(  apple.compareToIgnoreCase(grape)    );
+		// n1      n2
+		// a       a    = 0
+		// p       p    = 0
+		// p       p    = 0
+		// l       l    = 0
+		// a       g    = -6
+		
+		
+		//str1.comapreTo(  st2 ) ; 
+		
+		// negative ==> str1 comes first 
+		// 0        ==> if two string is identical 
+		// positive ==> str1 comes after 
+		
+		
+		
+		
+	}
+	
+	
+	static public String noWhiteSpace(String word) {
+		String[] words = word.split("\\W");
+		String result = "";
+		for(int i = 0; i < words.length; i++) {
+			result += words[i];
+		}
+		return result;
 	}
 
 }
